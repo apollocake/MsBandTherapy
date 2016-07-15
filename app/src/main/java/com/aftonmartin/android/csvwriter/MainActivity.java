29fb5ef0c0c2f049ac96a3db9676587e2e16d593
Afton Martin <aftonam@gmail.com>
2016-07-14 23:54:19 -0500

    added permissions and Band code for accelerometer

--- app/src/main/java/com/aftonmartin/android/csvwriter/MainActivity.java
+++ app/src/main/java/com/aftonmartin/android/csvwriter/MainActivity.java
@@ -5,6 +5,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +13,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
+
+import com.microsoft.band.BandClient;
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandIOException;
+import com.microsoft.band.BandInfo;
+import com.microsoft.band.ConnectionState;
+import com.microsoft.band.sensors.BandAccelerometerEvent;
+import com.microsoft.band.sensors.BandAccelerometerEventListener;
+import com.microsoft.band.sensors.SampleRate;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -42,8 +53,20 @@ public class MainActivity extends Activity {
     private FileOutputStream mFileOutputStream = null;
     private File mAccelFile = null;
     private File mGyroFile = null;
+    private BandClient client = null;
+
     float[] mSensorValues = null;
 
+
+    private BandAccelerometerEventListener mAccelerometerEventListener = new BandAccelerometerEventListener() {
+        @Override
+        public void onBandAccelerometerChanged(final BandAccelerometerEvent event) {
+            if (event != null) {
+                appendToUI(String.format(" X = %.3f \n Y = %.3f\n Z = %.3f", event.getAccelerationX(),
+                        event.getAccelerationY(), event.getAccelerationZ()));
+            }
+        }
+    };
 
     SensorEventListener mSensorEventListenerAccel = new SensorEventListener() {
         @Override
@@ -61,7 +84,8 @@ public class MainActivity extends Activity {
                             mAccelWriter.println("" + mSensorValues[0] + "," + mSensorValues[1] + "," + mSensorValues[2]);
                             textView1.post(new Runnable() {
                                 public void run() {
-                                    textView1.setText("x: " + mSensorValues[0] + "\ny: " + mSensorValues[1] + "\nz: " + mSensorValues[2]);                                 }
+                                    textView1.setText("x: " + mSensorValues[0] + "\ny: " + mSensorValues[1] + "\nz: " + mSensorValues[2]);
+                                }
                             });
                         }
                     }).start();
@@ -96,7 +120,8 @@ public class MainActivity extends Activity {
                             mGyroWriter.println("" + mSensorValues[0] + "," + mSensorValues[1] + "," + mSensorValues[2]);
                             textView2.post(new Runnable() {
                                 public void run() {
-                                    textView2.setText("x: " + mSensorValues[0] + "\ny: " + mSensorValues[1] + "\nz: " + mSensorValues[2]);                                 }
+                                    textView2.setText("x: " + mSensorValues[0] + "\ny: " + mSensorValues[1] + "\nz: " + mSensorValues[2]);
+                                }
                             });
                         }
                     }).start();
@@ -115,7 +140,6 @@ public class MainActivity extends Activity {
     };
 
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -128,30 +152,81 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         mStartButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                mResultsView.setText("");
                 checkPermissionsExplicit();
                 //blah blah GO ASYNC!
                 openSDFile();
                 registerListeners();
+                new AccelerometerSubscriptionTask().execute();
             }
         });
+        mResultsView.setText("");
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
+        if (client != null) {
+            try {
+                client.getSensorManager().unregisterAccelerometerEventListener(mAccelerometerEventListener);
+            } catch (BandIOException e) {
+                appendToUI(e.getMessage());
+            }
+        }
         unregisterListeners();
         closeSDFile();
     }
 
+    private class AccelerometerSubscriptionTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                if (getConnectedBandClient()) {
+                    appendToUI("Band is connected.\n");
+                    client.getSensorManager().registerAccelerometerEventListener(mAccelerometerEventListener, SampleRate.MS128);
+                } else {
+                    appendToUI("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
+                }
+            } catch (BandException e) {
+                String exceptionMessage = "";
+                switch (e.getErrorType()) {
+                    case UNSUPPORTED_SDK_VERSION_ERROR:
+                        exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.\n";
+                        break;
+                    case SERVICE_ERROR:
+                        exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
+                        break;
+                    default:
+                        exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
+                        break;
+                }
+                appendToUI(exceptionMessage);
 
+            } catch (Exception e) {
+                appendToUI(e.getMessage());
+            }
+            return null;
+        }
+    }
 
+    @Override
+    protected void onDestroy() {
+        if (client != null) {
+            try {
+                client.disconnect().await();
+            } catch (InterruptedException e) {
+                // Do nothing as this is happening during destroy
+            } catch (BandException e) {
+                // Do nothing as this is happening during destroy
+            }
+        }
+        super.onDestroy();
+    }
 
 
     private void registerListeners() {
@@ -174,10 +249,10 @@ public class MainActivity extends Activity {
                 mSensorManager.unregisterListener(mSensorEventListenerAccel);
                 mSensorManager.unregisterListener(mSensorEventListenerGyro);
             }
-        } catch (NullPointerException e){ //quickly exiting app after start causes mSensorManager to remain null
+        } catch (NullPointerException e) { //quickly exiting app after start causes mSensorManager to remain null
             e.printStackTrace();
             Log.i(TAG, "Listeners unregistered before registering");
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -245,7 +320,7 @@ public class MainActivity extends Activity {
                     " add a WRITE_EXTERNAL_STORAGE permission to the   manifest?");
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         mResultsView.append("\n\nFile written to " + mAccelFile + "\n");
@@ -277,5 +352,30 @@ public class MainActivity extends Activity {
             e.printStackTrace();
         }
         mResultsView.append("\n\nThat is all");
+    }
+
+    private void appendToUI(final String string) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mResultsView.setText(string);
+            }
+        });
+    }
+
+    private boolean getConnectedBandClient() throws InterruptedException, BandException {
+        if (client == null) {
+            BandInfo[] devices = BandClientManager.getInstance().getPairedBands();
+            if (devices.length == 0) {
+                appendToUI("Band isn't paired with your phone.\n");
+                return false;
+            }
+            client = BandClientManager.getInstance().create(getBaseContext(), devices[0]);
+        } else if (ConnectionState.CONNECTED == client.getConnectionState()) {
+            return true;
+        }
+
+        appendToUI("Band is connecting...\n");
+        return ConnectionState.CONNECTED == client.connect().await();
     }
 }
